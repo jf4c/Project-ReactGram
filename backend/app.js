38725714ca10fea94a0d6e3376b0c92db912ce0b
Julio Felipe <julio.f4costa@hotmail.com>
2023-05-08 16:34:45 -0300

    starting development of the backend part

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const port = process.env.PORT;
+const app = express();
+
+// config JSON and form data Response
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// Upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// DB conect
+require("./config/db.js");
+
+// Routes
+const router = require("./routes/Router.js");
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+});
